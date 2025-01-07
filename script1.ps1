be761054839173c2c23f717ddfa5741823f8913a
khimanshu1598 <kumar.himanshu@thoughtworks.com>
2025-01-07 16:41:27 +0530

    Create script1.ps1

---
+++ script1.ps1
@@ -0,0 +1,5 @@
+# Simulating the usage of parameters defined in config.ps1
+Write-Output "Running Script 1"
+Write-Output "CellName: $CellName"
+Write-Output "AnotherVar: $AnotherVar"
+Write-Output "DefaultVar: $DefaultVar"
